169ebcf48dacde7011d92f9d96bb1ff7c9b029ad
Vansh Gupta <vg2547@researchgrid.gsb.columbia.edu>
2022-07-22 10:17:32 -0400

    Updated span optimization

--- R/estimate_ste.R
+++ R/estimate_ste.R
@@ -9,8 +9,6 @@
 #' @param treatment Treatment variable.
 #' @param propensity Propensity score generated earlier.
 #' @param df The complete dataset.
-#' @param span_treated Optional span value for the treated model. Default is 0.75, which is the loess default.
-#' @param span_untreated Optional span value for the untreated model. Default is 0.75, which is the loess default.
 #' @returns df The same as input dataframe with the added column of the strategic treatment effect.
 #' @examples
 #' estimate_ste(
@@ -22,13 +20,23 @@
 #' @keywords
 #'  Strategic Treatment Effect
 #' @export
-estimate_ste <- function(y, treatment, propensity, df, span_treated = 0.75, span_untreated = 0.75) {
+estimate_ste <- function(y, treatment, propensity, df) {
     require(dplyr)
     # Setup models and variables.
     df$propensity <- propensity
     df$y <- y
-    model.treated <- loess(y ~ propensity, data = df %>% filter(treatment == 1), span = span_treated)
-    model.not_treated <- loess(y ~ propensity, data = df %>% filter(treatment == 0), span = span_untreated)
+    df_treated <- df %>% filter(treatment == 1)
+    df_untreated <- df %>% filter(treatment == 0)
+
+    # Find the optimal spans
+    span_treated = optim(par = c(0.5), calcSSE, method = "SANN", df = df_treated)
+    print(paste0("Span (Treated): ", span_treated))
+    span_untreated = optim(par = c(0.5), calcSSE, method = "SANN", df = df_untreated)
+    print(paste0("Span (Untreated): ", span_untreated))
+
+    # Run Local Regressions
+    model.treated <- loess(y ~ propensity, data = df_treated, span = span_treated)
+    model.not_treated <- loess(y ~ propensity, data = df_untreated, span = span_untreated)
 
     # Calculate predicted values.
     pos_prediction <- predict(model.treated, newdata = df)
@@ -42,5 +50,24 @@ estimate_ste <- function(y, treatment, propensity, df, span_treated = 0.75, span
 
     return(df)
 }
+
+# Function that returns the SSE
+calcSSE <- function(x, df){
+
+  loessMod <- try(loess(y ~ propensity, data = df, span = x), silent = T)
+  res <- try(loessMod$residuals, silent = T)
+
+  if(class(res) != "try-error"){
+    if((sum(res, na.rm = T) > 0)){
+      sse <- sum(res^2)
+    }
+  }else{
+    sse <- 99999
+  }
+
+  return(sse)
+}
+
+
 
 
